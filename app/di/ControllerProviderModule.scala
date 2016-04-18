a91e421a00cc9726152a9a1ccd1da30a4e5e5c04
Matt Russell <MattRussellUK@gmail.com>
2016-04-18 13:44:28 +0100

    Fix tests

--- app/di/ControllerProviderModule.scala
+++ app/di/ControllerProviderModule.scala
@@ -9,18 +9,18 @@ import play.api.Environment
 class ControllerProviderModule(environment: Environment, configuration: Configuration) extends AbstractModule {
 
   @Provides def controller(startup: Startup, factory: Factory): Application = factory.controller
-  @Provides def webApiController(factory: Factory) = factory.webApiController
-  @Provides def ciController(factory: Factory) = factory.ciController
-  @Provides def jsonController(factory: Factory) = factory.jsonController
-  @Provides def adminController(factory: Factory) = factory.adminController
-  @Provides def testController(factory: Factory) = factory.testController
-  @Provides def testsController(factory: Factory) = factory.testsController
-  @Provides def batchController(factory: Factory) = factory.batchController
-  @Provides def batchesController(factory: Factory) = factory.batchesController
-  @Provides def executionController(factory: Factory) = factory.executionController
-  @Provides def jenkinsController(factory: Factory) = factory.jenkinsController
-  @Provides def teamCityController(factory: Factory) = factory.teamCityController
-  @Provides def importLogController(factory: Factory) = factory.importLogController
+  @Provides def webApiController(startup: Startup, factory: Factory) = factory.webApiController
+  @Provides def ciController(startup: Startup, factory: Factory) = factory.ciController
+  @Provides def jsonController(startup: Startup, factory: Factory) = factory.jsonController
+  @Provides def adminController(startup: Startup, factory: Factory) = factory.adminController
+  @Provides def testController(startup: Startup, factory: Factory) = factory.testController
+  @Provides def testsController(startup: Startup, factory: Factory) = factory.testsController
+  @Provides def batchController(startup: Startup, factory: Factory) = factory.batchController
+  @Provides def batchesController(startup: Startup, factory: Factory) = factory.batchesController
+  @Provides def executionController(startup: Startup, factory: Factory) = factory.executionController
+  @Provides def jenkinsController(startup: Startup, factory: Factory) = factory.jenkinsController
+  @Provides def teamCityController(startup: Startup, factory: Factory) = factory.teamCityController
+  @Provides def importLogController(startup: Startup, factory: Factory) = factory.importLogController
 
   override def configure() {
 
